30b059ebeb49290216f675daf85df65e62406f14
AngeloCinaWD <angelo.cina@yahoo.it>
2021-04-10 16:55:07 +0200

    committt565665

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <p>hello</p>
+        <p>aaaa</p>
+    </div>
+@endsection
