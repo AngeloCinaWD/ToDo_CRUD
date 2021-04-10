4a8e92346add3a9d564fbbddd3da902a4e2f6067
AngeloCinaWD <angelo.cina@yahoo.it>
2021-04-10 17:10:52 +0200

    cbh

---
+++ laravel/resources/js/components/toDoComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <form>
+            <div class="input-group mb-3">
+                <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="button-addon2"
+                />
+                <button
+                    class="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                >
+                    Button
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    mounted() {}
+};
+</script>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
-        <p>hello</p>
-        <p>aaaa</p>
+        <h1 class="text-white display-4" >Vue.js ToDo App</h1>
+        <toDo-component></toDo-component>
     </div>
 @endsection
